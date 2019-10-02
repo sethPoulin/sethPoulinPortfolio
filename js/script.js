2e4984ff845249aa7cd960c5ed41b6f14d5b289a
Seth Poulin <seth.poulin@gmail.com>
2019-10-02 19:01:59 -0400

    figured out how to add waypoints and tested basic functionality.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+
+$(function(){
+  
+  const waypoint = new Waypoint({
+    element: document.getElementById('about'),
+    handler: function(){
+      // 
+      console.log('waypoints was fired');
+    }
+  })
+});
