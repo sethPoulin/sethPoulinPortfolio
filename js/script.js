93e697d1a3c4f3902af91e1c44c641669b84ad30
Seth Poulin <seth.poulin@gmail.com>
2019-10-23 12:05:44 -0400

    changed order of work sections and pushed dinner app further down.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 
 
 
-$(function(){
+// $(function(){
 
-  const waypoint = new Waypoint({
-    element: document.getElementById('about'),
-    handler: function(direction){
-      const nav = document.getElementById('navBar')
-      if (direction === 'up'){
-        nav.classList.add('show');
-      } else {
-        nav.classList.remove('show');
-      }
-    }
-  })
-});
+//   const waypoint = new Waypoint({
+//     element: document.getElementById('about'),
+//     handler: function(direction){
+//       const nav = document.getElementById('navBar')
+//       if (direction === 'up'){
+//         nav.classList.add('show');
+//       } else {
+//         nav.classList.remove('show');
+//       }
+//     }
+//   })
+// });
